7ec723a3dc1058fa6990e95cac7e6dea8d21a70a
Salil Apte <salil.apte99@gmail.com>
2018-06-29 23:03:33 +0530

    Add files via upload
    Cpp program to find area of triangle and rectangle using basic C++ Class concepts like keeping data members private, using two double values as class members, separate functions for input and compute area.

---
+++ Object Oriented Programming/area.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+class Dimension
+{
+private:
+	double a;
+	double b;
+public:
+	void TInput()
+	{
+		cout<<"Enter base of triangle:";
+		cin>>a;
+		cout<<"Enter height of triangle:";
+		cin>>b;
+	}
+	void RInput()
+	{
+		cout<<"Enter length of rectangle:";
+		cin>>a;
+		cout<<"Enter breadth of rectangle:";
+		cin>>b;
+	}
+
+	void ComputeTA()
+	{
+		double res1;
+		res1=((0.5)*(a)*(b));
+		cout<<"Area of triangle is:"<<res1<<endl;
+	}
+
+	void ComputeRA()
+	{
+		double res2;
+		res2=((a)*(b));
+		cout<<"Area of rectangle is:"<<res2<<endl;
+	}
+};
+
+int main()
+{
+	Dimension O1;
+	int choice;
+	cout<<"Select operation:\n1.Area of triangle\n2.Area of rectangle"<<endl;
+	cin>>choice;
+
+    switch(choice)
+    {
+    	case 1:
+    	{
+            O1.TInput();
+            O1.ComputeTA();
+            break;
+    	}
+        case 2:
+        {
+        	O1.RInput();
+            O1.ComputeRA();
+        }
+
+        default:
+        {
+        	cout<<"Invalid input.Please try again..."<<endl;
+        }
+    }
+    return 0;
+}
